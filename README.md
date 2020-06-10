01d29f69a7bf4ce571596b2048329d7065303585
Hasan Zaidi <haszaidi@gmail.com>
2020-06-10 20:58:46 +0100

    Add simple readme file.

---
+++ README.md
@@ -0,0 +1,7 @@
+# csharp-language-tester
+Repo for testing features of C#
+
+## Set up
+
+
+## Running
---
+++ csharp-language-tester/csharp-language-tester/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_language_tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
