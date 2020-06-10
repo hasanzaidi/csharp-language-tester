51a28299f45529e2ccb2ad183f9e8c7473dd3c13
Hasan Zaidi <hasan.zaidi@rocketmail.com>
2020-06-10 21:54:50 +0100

    Add comment.

--- CSharpLanguageTester/Program.cs
+++ CSharpLanguageTester/Program.cs
@@ -6,7 +6,7 @@ namespace CSharpLanguageTester
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Code snippets which can't really be expressed in unit test
         }
     }
 }
